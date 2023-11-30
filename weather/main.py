77f5c52c0c5ac99f9a45da5499b44e31cdf7a5b5
Jan Vincent Hoffbauer <jv@lewe-hoffbauer.de>
2023-11-30 15:48:17 +0100

    Fix folder structure and lstm hyperparam training (#52)
    Co-authored-by: Nick Bessin <Nick.Bessin@student.hpi.uni-potsdam.de>

--- weather/main.py
+++
@@ -1,5 +0,0 @@
-from dataloader import MeteostatDataLoader
-
-weather_data = MeteostatDataLoader(["weather_data_solar_stations.csv"], solar=True)
-weather_data.load_data()
-weather_data.preprocess_data()
