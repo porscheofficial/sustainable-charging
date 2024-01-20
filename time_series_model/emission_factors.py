6c95e42730950b211b3b4ab1535ac24dd3fa796e
Jan Vincent Hoffbauer <jv@lewe-hoffbauer.de>
2024-01-20 15:41:42 +0100

    Co2 eval (#81)
    Co-authored-by: Nick Bessin <Nick.Bessin@student.hpi.uni-potsdam.de>

---
+++ time_series_model/emission_factors.py
@@ -0,0 +1,15 @@
+# Emission factors in kg CO2e/MWh
+EMISSION_FACTORS = {
+    "biomass_mwh": 603.5,
+    "hydropower_mwh": 131.0,
+    "wind_offshore_mwh": 29.0,
+    "wind_onshore_mwh": 26.0,
+    "photovoltaic_mwh": 84.33333333333333,
+    "nuclear_mwh": 30.0,
+    "brown_coal_mwh": 1636.6,
+    "hard_coal_mwh": 1636.6,
+    "natural_gas_mwh": 952.6,
+    "pumped_storage_mwh": 82.5,
+    "other_conventional_mwh": 1294.6,
+    "other_renewables_mwh": 83.0,
+}
